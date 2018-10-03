6377b286fc9c855f555228e2140472c3e39ba85a
CFzS_NanohaS <igiko_ay@hotmail.com>
2018-10-03 18:01:33 +0700

    [Front] CallCentre2.1 link next page, call api and get param
    1. รอ api: POST
    2. เพิ่ม func: onSubmit next page
    3. get Value from URL

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,5 +4,6 @@ const api = require('./api');
 router.use('/api', api);
 
 router.get('/', (req, res) => res.sendfile('./views/th/call-centre-2.1.html'));
+router.get('/sms-sub', (req, res) => res.sendfile('./views/th/call-centre-2.2.html'));
 
 module.exports = router;
