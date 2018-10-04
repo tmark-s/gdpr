c3233e37f5d224584f909a51b792a4ad138cc255
CFzS_NanohaS <igiko_ay@hotmail.com>
2018-10-04 12:57:21 +0700

    [Front] Edit Subscrib-SMS
    รับค่าได้  แต่ api add ไม่ได้

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,18 @@ const api = require('./api');
 
 router.use('/api', api);
 
+
+// for call center
 router.get('/', (req, res) => res.sendfile('./views/th/call-centre-2.1.html'));
-router.get('/sms-sub', (req, res) => res.sendfile('./views/th/call-centre-2.2.html'));
+router.get('/unsubscribe', (req, res) => res.sendfile('./views/th/call-centre-2.2.html'));
+router.get('/snooze', (req, res) => res.sendfile('./views/th/call-centre-2.3.html'));
+router.get('/update-complete', (req, res) => res.sendfile('./views/th/call-centre-3.3.html'));
+router.get('/subscribe-sms', (req, res) => res.sendfile('./views/th/call-centre-3.1.html'));
+router.get('/subscribe-email', (req, res) => res.sendfile('./views/th/call-centre-3.2.html'));
+
+
+
+//for user
+router.post('/update-subscribe', (req, res) => res.send(req.body))
 
 module.exports = router;
