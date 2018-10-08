8c0f132e99d4aff9ece9ec0d1c5aa13ac3d80d3f
CFzS_NanohaS <igiko_ay@hotmail.com>
2018-10-08 17:53:30 +0700

    [front] หน้า Channel แก้ให้ดึงข้อมูลจาก db ได้

--- server/routes/index.js
+++ server/routes/index.js
@@ -9,11 +9,7 @@ router.get('/', async (req, res) => {
     const domain = await Domain.findOne({
         domainName: req.query.domain
     });
-    const user = await User.findOne({
-        info: {
-            link: req.query.link
-        }
-    });
+    const user = await User.findOne({ 'info.link': req.query.link });
 
     console.log(user);
     var hasPhone = false;
@@ -24,24 +20,23 @@ router.get('/', async (req, res) => {
         console.log("hasPhone: ", hasPhone)
         hasPhone = false
     }
-    /*
-        var hasEmail = false;
-        if (user.info.email != null || user.info.email != undefined) {
-            hasEmail = true;
-        } else {
-            hasEmail = false;
-        }
-    
-        res.render('channel', {
-            headText: "อัพเดทช่องทางรับข่าวสารจากบริษัทแสนสิริที่ท่านต้องการ",
-            hasEmailSubscribe: domain.channel.emailSubscribe,
-            hasSmsSubscribe: domain.channel.smsSubscribe,
-            hasPhoneSubscribe: domain.channel.phoneSubscribe,
-            hasPhone: hasPhone,
-            hasEmail: hasEmail,
-            hasOne: hasEmail || hasPhone
-        });
-        */
+
+    var hasEmail = false;
+    if (user.info.email != null || user.info.email != undefined) {
+        hasEmail = true;
+    } else {
+        hasEmail = false;
+    }
+
+    res.render('channel', {
+        headText: "อัพเดทช่องทางรับข่าวสารจากบริษัทแสนสิริที่ท่านต้องการ",
+        hasEmailSubscribe: domain.channel.emailSubscribe,
+        hasSmsSubscribe: domain.channel.smsSubscribe,
+        hasPhoneSubscribe: domain.channel.phoneSubscribe,
+        hasPhone: hasPhone,
+        hasEmail: hasEmail,
+        hasOne: hasEmail || hasPhone
+    });
 });
 
 module.exports = router;
