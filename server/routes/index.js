15a4f4e0569067bb1d8d1c56662f913bf1eb31b4
CFzS_NanohaS <igiko_ay@hotmail.com>
2018-10-08 16:56:05 +0700

    [Front] หน้า channel ปรับให้มีการดึง db

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,22 +6,42 @@ const User = require('../models/User');
 router.use('/api', api);
 
 router.get('/', async (req, res) => {
-  const domain = await Domain.findOne({
-    domainName: req.query.domain
-  });
-  const user = await User.find({
-    info: {
-      link: req.query.link
+    const domain = await Domain.findOne({
+        domainName: req.query.domain
+    });
+    const user = await User.findOne({
+        info: {
+            link: req.query.link
+        }
+    });
+
+    console.log(user);
+    var hasPhone = false;
+    if (user.info.phone != null || user.info.phone != undefined) {
+        hasPhone = true
+        console.log("hasPhone: ", hasPhone)
+    } else {
+        console.log("hasPhone: ", hasPhone)
+        hasPhone = false
     }
-  });
-  res.render('channel', {
-    headText: "อัพเดทช่องทางรับข่าวสารจากบริษัทแสนสิริที่ท่านต้องการ",
-    hasEmailSubscribe: domain.channel.emailSubscribe,
-    hasSmsSubscribe: domain.channel.smsSubscribe,
-    hasphoneSubscribe: domain.channel.phoneSubscribe,
-    phone: user.info.phone,
-    email: user.info.email
-  });
+    /*
+        var hasEmail = false;
+        if (user.info.email != null || user.info.email != undefined) {
+            hasEmail = true;
+        } else {
+            hasEmail = false;
+        }
+    
+        res.render('channel', {
+            headText: "อัพเดทช่องทางรับข่าวสารจากบริษัทแสนสิริที่ท่านต้องการ",
+            hasEmailSubscribe: domain.channel.emailSubscribe,
+            hasSmsSubscribe: domain.channel.smsSubscribe,
+            hasPhoneSubscribe: domain.channel.phoneSubscribe,
+            hasPhone: hasPhone,
+            hasEmail: hasEmail,
+            hasOne: hasEmail || hasPhone
+        });
+        */
 });
 
 module.exports = router;
