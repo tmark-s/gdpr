965dd8c5da3d9e8b120299915e4634576a6fe0be
CFzS_NanohaS <igiko_ay@hotmail.com>
2018-10-09 16:20:44 +0700

    [front] เพิ่มการสร้าง category สำหรับการแสดงข้อมูลในหน้า SMS และ Email

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,70 +6,88 @@ const User = require('../models/User');
 router.use('/api', api);
 
 async function getUserInfo(query) {
-    return await User.findOne(query);
+  return await User.findOne(query);
 
 }
 
 async function getDomainInfo(query) {
-    return await Domain.findOne(query);
+  return await Domain.findOne(query);
 }
 
 router.get('/', async (req, res) => {
-    const domain = await getDomainInfo({ 'domainName': req.query.domain })
-    const user = await getUserInfo({ 'info.link': req.query.link })
+  const domain = await getDomainInfo({ 'domainName': req.query.domain })
+  const user = await getUserInfo({ 'info.link': req.query.link })
 
-    var hasPhone = false;
-    if (user.info.phone) {
-        hasPhone = true
-        console.log("hasPhone: ", hasPhone)
-    } else {
-        console.log("hasPhone: ", hasPhone)
-        hasPhone = false
-    }
+  var hasPhone = false;
+  if (user.info.phone) {
+    hasPhone = true
+    console.log("hasPhone: ", hasPhone)
+  } else {
+    console.log("hasPhone: ", hasPhone)
+    hasPhone = false
+  }
 
-    var hasEmail = false;
-    if (user.info.email) {
-        hasEmail = true;
-    } else {
-        hasEmail = false;
-    }
+  var hasEmail = false;
+  if (user.info.email) {
+    hasEmail = true;
+  } else {
+    hasEmail = false;
+  }
 
-    // data for render Selected Channel Page.
-    res.render('channel', {
-        headText: "อัพเดทช่องทางรับข่าวสารจากบริษัทแสนสิริที่ท่านต้องการ",
-        hasEmailSubscribe: domain.channel.emailSubscribe,
-        hasSmsSubscribe: domain.channel.smsSubscribe,
-        hasPhoneSubscribe: domain.channel.phoneSubscribe,
-        hasPhone: hasPhone,
-        hasEmail: hasEmail,
-        hasOne: hasEmail || hasPhone,
-    });
+  // data for render Selected Channel Page.
+  res.render('channel', {
+    headText: "อัพเดทช่องทางรับข่าวสารจากบริษัทแสนสิริที่ท่านต้องการ",
+    hasEmailSubscribe: domain.channel.emailSubscribe,
+    hasSmsSubscribe: domain.channel.smsSubscribe,
+    hasPhoneSubscribe: domain.channel.phoneSubscribe,
+    hasPhone: hasPhone,
+    hasEmail: hasEmail,
+    hasOne: hasEmail || hasPhone,
+  });
 });
 
 router.get('/subscribe-sms', async (req, res) => {
-  const user = await User.findOne({ 
+  //// get user from database
+  const user = await User.findOne({
     'info.link': req.query.link
   });
 
+  //// get domain from database
   const domain = await Domain.findOne({
     domainName: req.query.domain
   });
-  
+
   const userDetail = user.domain.find((domain) => {
     return domain.domainName = req.query.domain
   });
 
+  ////  prepared data categoryList
+  const categories = domain.channel.smsSubscribe.categoryName;
+  const userCategory = userDetail.channel.smsSubscribe.smsSubscribeCategory;
+  const selectedCategory = []
+
+  categories.map(category => {
+    if (userCategory.indexOf(category) != -1) {
+      selectedCategory.push({ categoryName: category, categoryValue: category, Selected: true })
+    } else {
+      selectedCategory.push({ categoryName: category, categoryValue: category, Selected: false })
+    }
+  });
+
+  // render page
+
   res.render('subscribe-sms', {
     headText: "อัพเดทช่องทางรับข่าวสารจากบริษัทแสนสิริที่ท่านต้องการ",
     mobileNo: user.info.phone,
-    allCategory: domain.channel.smsSubscribe.categoryName,
+    allCategory: selectedCategory,
     ownCategory: userDetail.channel.smsSubscribe.smsSubscribeCategory,
     snooze: userDetail.channel.smsSubscribe.snooze
   });
 });
 
 router.get('/subscribe-email', async (req, res) => {
-  const user = await User.findOne({ 
+  debugger
+  const user = await User.findOne({
     'info.link': req.query.link
   });
 
@@ -81,10 +99,24 @@ router.get('/subscribe-email', async (req, res) => {
     return domain.domainName = req.query.domain
   });
 
+  const categories = domain.channel.emailSubscribe.categoryName;
+  const userCategory = userDetail.channel.emailSubscribe.emailSubscribeCategory;
+  const selectedCategory = []
+
+  console.log(categories)
+
+  categories.map(category => {
+    if (userCategory.indexOf(category) != -1) {
+      selectedCategory.push({ categoryName: category, categoryValue: category, Selected: true })
+    } else {
+      selectedCategory.push({ categoryName: category, categoryValue: category, Selected: false })
+    }
+  });
+
   res.render('subscribe-email', {
     headText: "อัพเดทช่องทางรับข่าวสารจากบริษัทแสนสิริที่ท่านต้องการ",
     email: user.info.email,
-    allCategory: domain.channel.emailSubscribe.categoryName,
+    allCategory: selectedCategory,
     ownCategory: userDetail.channel.emailSubscribe.emailSubscribeCategory,
     snooze: userDetail.channel.emailSubscribe.snooze
   });
