aa3801cc683a174f6e44a5d871dfcb8e9c2f5e81
Thanapon Sirisompark <tmark_s@hotmail.com>
2018-10-09 12:22:42 +0700

    Edit subscribe-email

--- server/routes/index.js
+++ server/routes/index.js
@@ -55,7 +55,8 @@ router.get('/subscribe-sms', async (req, res) => {
     headText: "อัพเดทช่องทางรับข่าวสารจากบริษัทแสนสิริที่ท่านต้องการ",
     mobileNo: user.info.phone,
     allCategory: domain.channel.smsSubscribe.categoryName,
-    ownCategory: user.domain[0].channel.smsSubscribeCategory
+    ownCategory: user.domain[0].channel.smsSubscribe.smsSubscribeCategory,
+    isSnooze: user.domain[0].channel.smsSubscribe.isSnooze
   });
 });
 
@@ -71,10 +72,10 @@ router.get('/subscribe-email', async (req, res) => {
 
   res.render('subscribe-email', {
     headText: "อัพเดทช่องทางรับข่าวสารจากบริษัทแสนสิริที่ท่านต้องการ",
-    mobileNo: user.info.phone,
-    allCategory: domain.channel.smsSubscribe.categoryName,
-    ownCategory: user.domain[0].channel.smsSubscribe.smsSubscribeCategory,
-    isSnooze: user.domain[0].channel.smsSubscribe.isSnooze
+    email: user.info.email,
+    allCategory: domain.channel.emailSubscribe.categoryName,
+    ownCategory: user.domain[0].channel.emailSubscribe.emailSubscribeCategory,
+    isSnooze: user.domain[0].channel.emailSubscribe.isSnooze
   });
 });
 
