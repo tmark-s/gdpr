36f14b349f3e80ac11b2441a7478afb6ecf4e6f6
Thanapon Sirisompark <tmark_s@hotmail.com>
2018-10-11 12:02:26 +0700

    Add create domain

--- server/controllers/DomainController.js
+++ server/controllers/DomainController.js
@@ -13,6 +13,31 @@ exports.find = async (req, res) => {
 };
 
 exports.create = async (req, res) => {
+  const findDomain = await Domain.findOne({
+    name: req.body.domain
+  });
+
+  if (!findDomain) {
+    const newDomain = new Domain();
+    newDomain.name = req.body.domain;
+    newDomain.emailSubscribe = {
+      canSubscribe: false,
+      category: []
+    };
+    newDomain.smsSubscribe = {
+      canSubscribe: false,
+      category: []
+    };
+    newDomain.phoneSubscribe = {
+      canSubscribe: false
+    };
+    await newDomain.save();
+    res.json(newDomain);
+  }
+  else {
+    res.status(500).json("Domain already exist");
+  }
+
   try {
     const newDomain = new Domain(req.body);
     await newDomain.save();
--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -3,53 +3,62 @@ const Domain = require('../models/Domain');
 const hash = require('object-hash');
 
 exports.create = async (req, res) => {
-    
+  const findUser = await User.findOne({
+    'info.email': req.body.email
+  });
+
+  if (!findUser) {
     const domain = await Domain.findOne({
       name: req.body.domain
     });
-
+  
     const emailSubscribe = [];
     await domain.emailSubscribe.category.map((category) => {
       emailSubscribe.push(category.value);
     });
-  
+    
     const smsSubscribe = [];
     await domain.smsSubscribe.category.map((category) => {
       smsSubscribe.push(category.value);
     });
-
+  
     const snooze = {
       isSnooze: false,
       startDate: "",
       endDate: ""
     };
-
+  
     const addDomain = {};
     addDomain.name = req.body.domain;
     addDomain.emailSubscribe = emailSubscribe;
     addDomain.smsSubscribe = smsSubscribe;
+    addDomain.phoneSubscribe = false
     addDomain.snooze = snooze;
-
+  
     const userDetail = [];
     userDetail.push(addDomain);
-
+  
     const newUser = new User();
     newUser.domain = userDetail;
-
+  
     await newUser.save();
-
+  
     const user = hash(newUser._id.toString());
-
+  
     const info = {
       email: req.body.email,
       user: user
     };
-
+  
     newUser.info = info;
-
+  
     await newUser.save();
-
+  
     res.json(newUser);
+  }
+  else {
+    res.status(500);
+  }
 };
 
 exports.find = async (req, res) => {
