b108ee326b689df2d4a177e6b62d3071cd28e66b
Thanapon Sirisompark <tmark_s@hotmail.com>
2018-10-11 11:03:06 +0700

    Edit create user

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,14 +1,55 @@
 const User = require('../models/User');
+const Domain = require('../models/Domain');
+const hash = require('object-hash');
 
 exports.create = async (req, res) => {
-  try {
-    const newUser = new User(req.body);
+    
+    const domain = await Domain.findOne({
+      name: req.body.domain
+    });
+
+    const emailSubscribe = [];
+    await domain.emailSubscribe.category.map((category) => {
+      emailSubscribe.push(category.value);
+    });
+  
+    const smsSubscribe = [];
+    await domain.smsSubscribe.category.map((category) => {
+      smsSubscribe.push(category.value);
+    });
+
+    const snooze = {
+      isSnooze: false,
+      startDate: "",
+      endDate: ""
+    };
+
+    const addDomain = {};
+    addDomain.name = req.body.domain;
+    addDomain.emailSubscribe = emailSubscribe;
+    addDomain.smsSubscribe = smsSubscribe;
+    addDomain.snooze = snooze;
+
+    const userDetail = [];
+    userDetail.push(addDomain);
+
+    const newUser = new User();
+    newUser.domain = userDetail;
+
     await newUser.save();
+
+    const user = hash(newUser._id.toString());
+
+    const info = {
+      email: req.body.email,
+      user: user
+    };
+
+    newUser.info = info;
+
+    await newUser.save();
+
     res.json(newUser);
-  }
-  catch (error) {
-    res.status(500).json(error);
-  }
 };
 
 exports.find = async (req, res) => {
